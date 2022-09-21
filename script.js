a465f9d7edc58d13a2df680249a6a84a83f89064
skolotaja <94806629+skolotaja@users.noreply.github.com>
2022-09-22 00:35:39 +0300

    Otrais

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function iegutDarglietasNoApi() {
-    let datiNoApi=fetch('https://fakestoreapi.com/products/category/jewelery');
-    let datiJson=await datiNoApi;
-    console.log(datiJson);
+async function iegutDarglietasNoApi() {
+    let datiNoApi=await fetch('https://fakestoreapi.com/products/category/jewelery');
+    let datiJson=datiNoApi.json;
+    //console.log(datiJson);
+    return datiJson;
     
+}
+async function iegutApgerbus() {
+    let datiNoApi=await fetch('https://fakestoreapi.com/products/category/women%20clothing');
+    let datiJson=datiNoApi.json;
+    //console.log(datiJson);
+    return datiJson;
 }
