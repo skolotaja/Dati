0abd2a84f944c2727899d0d39f7c6d7a297afa9b
skolotaja <94806629+skolotaja@users.noreply.github.com>
2022-09-25 22:55:51 +0300

    adrese

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ async function iegutDarglietasNoApi() {
     //return datiJson;
     
 }
-//async function iegutApgerbus() {
-    //let datiNoApi=await fetch('https://fakestoreapi.com/products/category/women%20clothing');
-    //let datiJson=datiNoApi.json;
-    //console.log(datiJson);
-    ////return datiJson;
-//}
+async function iegutApgerbusNoApi() {
+    let datiNoApi=await fetch('https://fakestoreapi.com/products/category/women%20clothing');
+    let datiJson= await datiNoApi.json;
+    console.log(datiJson);
+    //return datiJson;
+}
+async function iegutdarbaLaiksNoApi() {
+    let datiNoApi=await fetch('https://skolotaja.github.io/Dati/darbaLaiks.json');
+    let datiJson= await datiNoApi.json;
+    console.log(datiJson);
+    //return datiJson;
+}
