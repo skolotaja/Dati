d0f9dad66eba025ce27f47190c63208fd60a01af
skolotaja <94806629+skolotaja@users.noreply.github.com>
2022-09-25 23:14:22 +0300

    nakamais

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 async function iegutDarglietasNoApi() {
     let datiNoApi=await fetch('https://fakestoreapi.com/products/category/jewelery');
     let datiJson=await datiNoApi.json;
-    console.log(datiJson);
-    //return datiJson;
+    //console.log(datiJson);
+    return datiJson;
     
 }
 async function iegutApgerbusNoApi() {
     let datiNoApi=await fetch('https://fakestoreapi.com/products/category/women%20clothing');
     let datiJson= await datiNoApi.json;
-    console.log(datiJson);
-    //return datiJson;
+    //console.log(datiJson);
+    return datiJson;
 }
 async function iegutdarbaLaiksNoApi() {
     let datiNoApi=await fetch('https://skolotaja.github.io/Dati/darbaLaiks.json');
     let datiJson= await datiNoApi.json;
-    console.log(datiJson);
-    //return datiJson;
+    //console.log(datiJson);
+    return datiJson;
 }
+async function raditDatus() {
+    let darglietasJson=await iegutDarglietasNoApi();
+    let apgerbiJson= await iegutApgerbusNoApi();
+    let produktiKopa=darglietasJson.concat(apgerbiJson);
+    console.log(produktiKopa);
+}
+raditDatus();
