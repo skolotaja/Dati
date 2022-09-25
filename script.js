d26f3499af4aa6edad1badf0d45c06e9686fdf8e
skolotaja <94806629+skolotaja@users.noreply.github.com>
2022-09-25 23:48:26 +0300

    gatavs

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 async function iegutDarglietasNoApi() {
     let datiNoApi=await fetch('https://fakestoreapi.com/products/category/jewelery');
-    let datiJson=await datiNoApi.json;
+    let datiJson=datiNoApi.json;
     //console.log(datiJson);
     return datiJson;
     
 }
 async function iegutApgerbusNoApi() {
     let datiNoApi=await fetch('https://fakestoreapi.com/products/category/women%20clothing');
-    let datiJson= await datiNoApi.json;
+    let datiJson= datiNoApi.json;
     //console.log(datiJson);
     return datiJson;
 }
@@ -22,5 +22,9 @@ async function raditDatus() {
     let apgerbiJson= await iegutApgerbusNoApi();
     let produktiKopa=darglietasJson.concat(apgerbiJson);
     console.log(produktiKopa);
-}
+    for (let i =0;i<produktiKopa.length;i++)
+        {
+            console.log(produktiKopa[i]['title']); 
+    }
 raditDatus();
+}
